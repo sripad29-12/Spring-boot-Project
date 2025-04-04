5616a75b46114787e3e81d5a55fdfcce4bde8afe
sripad29-12 <sripad.metpelly@gmail.com>
2025-04-04 18:52:31 +0530

    Initial commit

---
+++ demo/src/main/java/com/demonstration/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.demonstration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("/")
+	public String getName(){
+		return "Congratulations, you deployed your first spring boot application on AWS";
+	}
+
+}
